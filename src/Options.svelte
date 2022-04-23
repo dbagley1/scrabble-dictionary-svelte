d629805b67664f4fbc6b9ecc1d57b176f12add0e
Darrian Bagley <dbagle14@gmail.com>
2022-04-23 08:50:15 -0400

    production build

---
+++ src/Options.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { optionsStore } from "./stores.js";
+
+  export let showDefinition = true;
+</script>
+
+<div class="options-container">
+  <label for="showDefinition">
+    <input type="checkbox" name="showDefinition" bind:value={showDefinition} on:change={optionsStore.toggleDefinition(showDefinition)} />
+    Show Definitions
+  </label>
+</div>
